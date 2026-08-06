42063a40a404832895ffa70dde3887206cc4bc39
Vishnu <vishnupraks@gmail.com>
2026-08-05 20:09:25 -0700

    chore: update Gemini model version to 2.0-flash-lite in API request URL

--- analyze.php
+++ analyze.php
@@ -282,7 +282,7 @@ PROMPT;
 
 $url =
 
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash-lite:generateContent";
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent";
 
 
 
