f862796cee8ca65b9cee8ab78840117aa755f331
Vishnu <vishnupraks@gmail.com>
2026-08-07 11:33:43 -0700

    feat: update Google OAuth configuration and replace auto-redirect with manual connection link

--- google_login.php
+++ google_login.php
@@ -4,27 +4,45 @@ require_once 'vendor/autoload.php';
 
 session_start();
 
+
 $client = new Google_Client();
 
-$client->setAuthConfig('client_secret.json');
+
+$client->setAuthConfig(
+    'client_secret.json'
+);
+
 
 $client->setRedirectUri(
     'https://vishthefishjr.me/oauth_callback.php'
 );
 
+
 $client->addScope(
     Google_Service_Slides::PRESENTATIONS
 );
+
 
 $client->addScope(
     Google_Service_Drive::DRIVE_FILE
 );
 
 
+// Important: keep login active
+$client->setAccessType('offline');
+
+
 $url = $client->createAuthUrl();
 
-header("Location: " . $url);
 
-exit;
+echo "
+
+<a href='$url'>
+
+Connect Google Account
+
+</a>
+
+";
 
 ?>
