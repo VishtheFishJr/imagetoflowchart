8f055af28c3981ee9631a35220a9efad335fd1ad
Vishnu <vishnupraks@gmail.com>
2026-08-07 11:06:01 -0700

    feat: implement Google OAuth2 authentication flow for user login

---
+++ google_login.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+session_start();
+
+$client = new Google_Client();
+
+$client->setAuthConfig('client_secret.json');
+
+$client->setRedirectUri(
+    'https://vishthefishjr.me/oauth_callback.php'
+);
+
+$client->addScope(
+    Google_Service_Slides::PRESENTATIONS
+);
+
+$client->addScope(
+    Google_Service_Drive::DRIVE_FILE
+);
+
+
+$url = $client->createAuthUrl();
+
+header("Location: " . $url);
+
+exit;
+
+?>
