79a5c1f615e05e5969d434bf4675342be98199a1
Vishnu <vishnupraks@gmail.com>
2026-08-07 11:28:34 -0700

    refactor: update API key retrieval to use environment variables instead of hardcoded values

--- test.php
+++ test.php
@@ -1,16 +1,5 @@
 <?php
 
-$apiKey = "AQ.Ab8RN6LQwwMMPp9NQdqOsUvG1zSm-DoUCc85bJ0FDiEj7q5Ceg";
+echo getenv("GEMINI_API_KEY");
 
-$url = "https://generativelanguage.googleapis.com/v1beta/models";
-
-$ch = curl_init($url);
-
-curl_setopt_array($ch, [
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HTTPHEADER => [
-        "x-goog-api-key: $apiKey"
-    ]
-]);
-
-echo curl_exec($ch);
+?>
