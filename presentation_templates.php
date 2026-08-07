585cb1180ebb54b1a47f0bc227e8263ab1346f4b
Vishnu <vishnupraks@gmail.com>
2026-08-06 23:15:20 -0700

    refactor: replace static object IDs with dynamic unique identifiers and consolidate slide text insertion logic

--- presentation_templates.php
+++ presentation_templates.php
@@ -4,40 +4,66 @@
 function titleSlide($slide, $title, $subtitle)
 {
 
+    $titleId = uniqid("title_");
+
+
     return [
 
         [
             "createShape" => [
-                "objectId" => "title_box",
+
+                "objectId" => $titleId,
+
                 "shapeType" => "TEXT_BOX",
+
                 "elementProperties" => [
+
                     "pageObjectId" => $slide,
+
                     "size" => [
+
                         "width" => [
                             "magnitude" => 6000000,
                             "unit" => "EMU"
                         ],
+
                         "height" => [
                             "magnitude" => 1000000,
                             "unit" => "EMU"
                         ]
+
                     ],
+
                     "transform" => [
+
                         "scaleX" => 1,
+
                         "scaleY" => 1,
+
                         "translateX" => 800000,
+
                         "translateY" => 1500000,
+
                         "unit" => "EMU"
+
                     ]
+
                 ]
+
             ]
+
         ],
+
 
         [
             "insertText" => [
-                "objectId" => "title_box",
-                "text" => $title
+
+                "objectId" => $titleId,
+
+                "text" => $title . "\n\n" . $subtitle
+
             ]
+
         ]
 
     ];
@@ -50,8 +76,10 @@ function titleSlide($slide, $title, $subtitle)
 function bulletSlide($slide, $title, $points)
 {
 
-    $text =
-        $title . "\n\n";
+    $textboxId = uniqid("bullet_");
+
+
+    $text = $title . "\n\n";
 
 
     foreach ($points as $point) {
@@ -65,8 +93,11 @@ function bulletSlide($slide, $title, $points)
     return [
 
         [
+
             "createShape" => [
-                "objectId" => uniqid("textbox_"),
+
+                "objectId" => $textboxId,
+
                 "shapeType" => "TEXT_BOX",
 
                 "elementProperties" => [
@@ -74,25 +105,47 @@ function bulletSlide($slide, $title, $points)
                     "pageObjectId" => $slide,
 
                     "size" => [
+
                         "width" => [
                             "magnitude" => 6000000,
                             "unit" => "EMU"
                         ],
+
                         "height" => [
                             "magnitude" => 4000000,
                             "unit" => "EMU"
                         ]
+
                     ],
 
                     "transform" => [
+
                         "scaleX" => 1,
+
                         "scaleY" => 1,
+
                         "translateX" => 700000,
+
                         "translateY" => 800000,
+
                         "unit" => "EMU"
+
                     ]
 
                 ]
+
+            ]
+
+        ],
+
+
+        [
+
+            "insertText" => [
+
+                "objectId" => $textboxId,
+
+                "text" => $text
 
             ]
 
@@ -101,5 +154,6 @@ function bulletSlide($slide, $title, $points)
     ];
 
 }
+
 
 ?>
